d2fa0ab06bd26a719e806088dd5dc7a1580e53ae
OksanaZhmurko <oksanazhmurko@gmail.com>
2023-02-12 21:28:42 +0200

    task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,10 +4,6 @@ const output = document.querySelector('#name-output')
 textInput.addEventListener('input', event => {
 	output.textContent = event.target.value
 
-	if (event.target.value === '') {
-		output.textContent = 'Anonymous'
+	output.textContent = event.target.value || "Anonymous"
 	}
-})
-
-console.log(textInput);
-console.log(output);
+)
